60d73a709b6160ce63c03e663921d8fb984eeae6
Hoang Thien <thienprola456@gmail.com>
2026-05-14 14:37:29 +0700

    Add project files.

---
+++ CMS.Backend/Controllers/PostController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMS.Data.Entities;
+
+namespace CMS.Backend.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            var posts = new List<Post>
+            {
+                new Post
+                {
+                    Id = 1,
+                    Title = "Lộ trình học ASP.NET Core",
+                    Content = "Hướng dẫn học ASP.NET Core từ cơ bản đến nâng cao.",
+                    ImageUrl = "https://via.placeholder.com/300",
+                    CreatedDate = DateTime.Now
+                },
+
+                new Post
+                {
+                    Id = 2,
+                    Title = "Cài đặt ReactJS",
+                    Content = "Hướng dẫn cài đặt ReactJS cho người mới.",
+                    ImageUrl = "https://via.placeholder.com/300",
+                    CreatedDate = DateTime.Now.AddDays(-1)
+                }
+            };
+
+            return View(posts);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var post = new Post
+            {
+                Id = id,
+                Title = "Chi tiết bài viết " + id,
+                Content = "Đây là nội dung chi tiết của bài viết.",
+                ImageUrl = "https://via.placeholder.com/600x300",
+                CreatedDate = DateTime.Now
+            };
+
+            return View(post);
+        }
+    }
+}
---
+++ CMS.Data/Entities/Category.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Sinh Vien:Pham Nguyen Hoang Thien
+ * Ma sv:2123110028
+ * Ngay tao:14-05-2026
+ * Version:1.0
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    //thuc the danh muc bai viet
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+       
+
+        //quan he 1-n voi bai viet
+        public ICollection<Post> Posts { get; set; }
+    }
+}
